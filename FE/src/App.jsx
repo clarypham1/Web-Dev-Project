06373fc408b37dffe96dbc63501d8fc04cea4dcc
Yamong Phoo <yamongphoo012@gmail.com>
2026-09-13 18:11:43 +0300

    feat: add login Page

---
+++ FE/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Login from './Components/login-page/LoginPage.jsx';
+import "./Components/login-page/LoginPage.css";
+import "./Components/login-page/Header.css";
+import "./Components/login-page/Footer.css";
+
+
+function App () {
+  return (
+    <div>
+      <Login/>
+    </div>
+
+  );
+}
+
+export default App
