d6a61f38f7d1a8215217965099f7553ae34a1514
Saga <sagalehtonen@gmail.com>
2026-09-16 11:33:12 +0300

    add user imports and app.use(userstuff) to server.js

--- Saga/BE/server.js
+++ Saga/BE/server.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import itemRouter from "./routes/itemRouter.js";
+import userRouter from "./routes/userRouter.js"
+
 console.log("ITEM ROUTER LOADED");
 
 dotenv.config();
@@ -16,6 +18,7 @@ app.use(express.json());
 
 
 app.use("/items", itemRouter)
+app.use("/users", userRouter)
 
 
 //test route
